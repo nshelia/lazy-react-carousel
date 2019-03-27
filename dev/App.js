d91cd7da5b38bc0d2aa6d747f62ddd6e031c191b
Nika Shelia <nshprimary@gmail.com>
2019-03-27 16:09:21 +0400

    Add switches in demo example to manipulate carousel props

--- dev/App.js
+++ dev/App.js
@@ -4,8 +4,14 @@ import leftArrow from "assets/left-arrow.svg"
 import rightArrow from "assets/right-arrow.svg"
 import { hot } from 'react-hot-loader/root'
 import Button from '@material-ui/core/Button';
+import Switch from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 function Application() {
+	const [parameters,setParameters] = useState({
+		lazy: true, showCounter: true, showArrows: true
+	})
 
 	const [posters, setPosters] = useState([])
 
@@ -63,6 +69,42 @@ function Application() {
 				<h1>Lazy React Carousel </h1>
 				<h4>Light-weight and customizable horizontal carousel for React apps :)</h4>
 				<div className="carousel-methods">
+					<FormGroup row classes={{root: 'carousel-switch-group'}}>
+		        <FormControlLabel
+		          control={
+		            <Switch
+		            	classes={{ bar: 'carousel-switch-bar' }}
+		              checked={parameters.lazy}
+		              onChange={() => setParameters({...parameters, lazy: !parameters.lazy})}
+		            />
+		          }
+		          classes={{ label: 'carousel-switch-label' }}
+		          label="Use lazy load"
+		        />
+		        <FormControlLabel
+		          control={
+		            <Switch
+		            	classes={{ bar: 'carousel-switch-bar' }}
+		              checked={parameters.showArrows}
+		              onChange={() => setParameters({...parameters, showArrows: !parameters.showArrows})}
+		            />
+		          }
+		          classes={{ label: 'carousel-switch-label' }}
+		          label="Hide/Show Arrows"
+		        />
+		        <FormControlLabel
+		          control={
+		            <Switch
+		            	classes={{ bar: 'carousel-switch-bar' }}
+		              checked={parameters.showCounter}
+		              onChange={() => setParameters({...parameters, showCounter: !parameters.showCounter})}
+		            />
+		          }
+		          classes={{ label: 'carousel-switch-label' }}
+		          label="Hide/Show Counter"
+		        />
+	      	</FormGroup>
+					
 					<Button variant="contained" color="primary" onClick={() => ref.current.scrollToSlide(1)}>go to slide 1</Button>
 					<Button variant="contained" color="primary" onClick={() => ref.current.scrollToSlide(2)}>go to slide 2</Button>
 					<Button variant="contained" color="primary" onClick={() => ref.current.scrollToSlide(3)}>go to slide 3</Button>
@@ -73,10 +115,13 @@ function Application() {
 				<div className="col-12">
 					<Carousel
 						scrollDuration={1000}
-						itemsPerSlide={2}
-						itemsToScroll={2}
+						itemsPerSlide={3}
+						itemsToScroll={3}
 						nextArrow={nextArrow}
 						prevArrow={prevArrow}
+						showCounter={parameters.showCounter}
+						showArrows={parameters.showArrows}
+						lazy={parameters.lazy}
 						ref={ref}
 					>
 						{renderList()}
